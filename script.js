cb66dd4be8520daac428e3dffba4e1be5cd148bf
Nathan <logonrmlocal@fiap.com.br>
2024-04-26 09:27:11 -0300

    Estilazando e criando o JavaScript

---
+++ script.js
@@ -0,0 +1,7 @@
+let p1=prompt("Digite seu NOME")
+let p2=prompt("Digite seu PODER")
+let p3=prompt("Digite o nome de um Vilão")
+let p4=prompt("digite um LUGAR")
+let msg=document.getElementById("msg")
+
+msg.innerHTML=`ola ${p1}, o seu poder é ${p2} e voce vai enfrentar o(a) ${p3} no(em) ${p4}`
